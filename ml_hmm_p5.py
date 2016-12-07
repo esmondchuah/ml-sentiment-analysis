9118437ab5d54e20e35c7629c8661881b66ca52a
Esmond Chuah <esmondchuah@gmail.com>
2016-12-07 16:04:43 +0800

    refactored code for part 5

--- ml_hmm_p5.py
+++ ml_hmm_p5.py
@@ -1,13 +1,15 @@
 import sys
 sys.setrecursionlimit(2000)
 
-possible_states = ["O","B-positive","I-positive","B-neutral","I-neutral","B-negative","I-negative"]
+possible_states = ["O", "B-positive", "I-positive", "B-neutral", "I-neutral", "B-negative", "I-negative"]
 
+
+# a modified Data_processor class which performs some pre-processing on the training data before returning the formatted array of data.
 class Data_processor:
-    def __init__(self,path):
-        self.datal= []
+    def __init__(self, path):
+        self.datal = []
         self.data = []
-        self.file = open(path,'r',encoding="utf8")
+        self.file = open(path, 'r', encoding="utf8")
         for i in self.file.read().split("\n\n") :
             sentence = []
             unlabeled = []
@@ -18,7 +20,7 @@ class Data_processor:
                     if len(word) > 5 and word[:4] == "http":
                         word = word[:4]
                     if len(j.split(" ")) >1:
-                        sentence.append(word + " " +j.split(" ")[-1])
+                        sentence.append(word + " " + j.split(" ")[-1])
                     else:
                         sentence.append(word)
             if len(sentence) > 0:
@@ -26,184 +28,210 @@ class Data_processor:
                 self.datal.append(unlabeled)
         self.file.close()
 
-def emis_prob(a,b,Data,data_dict):
-    if (a,b) in data_dict.keys():
-        return data_dict[(a,b)]
+
+def emis_prob(state, word, training_data, emis_dict):
+    if (state, word) in emis_dict.keys():
+        return emis_dict[(state, word)]
     else:
-        countAB = 0
-        countA = 1
-        countB = 0
-        for tweet in Data.data:
+        count_emission = 0 # count the emission from state to word
+        count_state = 1
+        count_word = 0
+
+        for tweet in training_data:
             for j in range(len(tweet)):
-                if tweet[j].split(" ")[0] == b:
-                    countB +=1
-                if tweet[j].split(" ")[1] == a:
-                    countA +=1
-                    if tweet[j].split(" ")[0] == b:
-                        countAB +=1
-        if countB == 0:
-            result =  float(1/countA)
+                if tweet[j].split(" ")[0] == word:
+                    count_word += 1
+                if tweet[j].split(" ")[1] == state:
+                    count_state += 1
+                    if tweet[j].split(" ")[0] == word:
+                        count_emission += 1
+
+        if count_word == 0:
+            result =  float(1/count_state)
         else:
-            result = float(countAB/countA)
-        data_dict[(a,b)] = result
+            result = float(count_emission/count_state)
+
+        emis_dict[(state, word)] = result
         return result
 
-def transAB_prob(a,b,Data,data_dict):
-    if (a,b) in data_dict.keys():
-        return data_dict[(a,b)]
+
+# 'a' represents 'state1' and 'b' represents 'state2'
+def transAB_prob(a, b, training_data, trans_dict):
+    if (a,b) in trans_dict.keys():
+        return trans_dict[(a,b)]
     else:
         countAB = 0
         countA = 0
+
         if a == 'start':
-            countA = len(Data.data)
-            for tweet in Data.data:
+            countA = len(training_data)
+            for tweet in training_data:
                 if len(tweet[0].split(" ")) > 1 and tweet[0].split(" ")[1] == b:
                     countAB += 1
         elif b == 'stop':
-            for tweet in Data.data:
+            for tweet in training_data:
                 for j in range(len(tweet)):
                     if len(tweet[j].split(" ")) > 1 and tweet[j].split(" ")[1] == a:
-                        countA +=1
-                        if j == len(tweet)-1:
-                            countAB +=1
+                        countA += 1
+                        if j == len(tweet) - 1:
+                            countAB += 1
         elif a == 'stop' or b == 'start':
-            data_dict[(a,b)] = 0
+            trans_dict[(a,b)] = 0
             return 0
         else:
-            for tweet in Data.data:
+            for tweet in training_data:
                 for j in range(len(tweet)-1):
                     if len(tweet[j].split(" ")) > 1 and tweet[j].split(" ")[1] == a:
-                        countA +=1
+                        countA += 1
                         if tweet[j+1].split(" ")[1] == b:
-                            countAB +=1
+                            countAB += 1
+
         result = float(countAB/countA)
-        data_dict[(a,b)] = result
+        trans_dict[(a,b)] = result
         return result
 
 
-def trans_prob_ABC(a,b,c,Data,data_dict):
-    if (a,b,c) in data_dict.keys():
-        return data_dict[(a,b,c)]
+# 'a' represents 'state1', 'b' represents 'state2' and 'c' represents 'state3'
+def trans_prob_ABC(a, b, c, training_data, trans_dict):
+    if (a,b,c) in trans_dict.keys():
+        return trans_dict[(a,b,c)]
     else:
         countABC = 0
         countAB = 0
+
         if a == "start":
-            for tweet in Data.data:
+            for tweet in training_data:
                 if len(tweet[0].split(" ")) > 1 and tweet[0].split(" ")[1] == b:
                     countAB += 1
                     if tweet[1].split(" ")[1] == c:
-                        countABC +=1
+                        countABC += 1
         elif c == "stop":
-            for tweet in Data.data:
+            for tweet in training_data:
                 for j in range(1,len(tweet)):
                     if len(tweet[j].split(" ")) > 1 and tweet[j].split(" ")[1] == b and tweet[j-1].split(" ")[1] == c:
                         countAB += 1
-                        if j == len(tweet)-1:
-                            countABC +=1
-        elif a=="stop" or b == "stop" or b == "start" or c == "start":
-            data_dict[(a,b,c)] = 0
+                        if j == len(tweet) - 1:
+                            countABC += 1
+        elif a == "stop" or b == "stop" or b == "start" or c == "start":
+            trans_dict[(a,b,c)] = 0
             return 0
         else:
-            for tweet in Data.data:
+            for tweet in training_data:
                 for j in range(1,len(tweet)-1):
                     if len(tweet[j].split(" ")) > 1 and tweet[j].split(" ")[1] == b and tweet[j-1].split(" ")[1] == a:
                         countAB += 1
                         if tweet[j+1].split(" ")[1] == c:
-                            countABC +=1
+                            countABC += 1
+
         if countAB == 0:
             result = 0
         else:
             result = float(countABC/countAB)
-        data_dict[(a,b,c)] = result
+
+        trans_dict[(a,b,c)] = result
         return result
 
 
-def viterbip5_label(inpathdev,Datapath,filename,os):
-    Data = Data_processor(Datapath)
+def viterbip5_label(dev_datapath, training_datapath, filename, os):
     if os == "W":
-        outpathdev = inpathdev.rsplit("\\",maxsplit=1)[0] + "\\" + filename)
+        outpath = dev_datapath.rsplit("\\",maxsplit=1)[0] + "\\" + filename
     else:
-        outpathdev = inpathdev.rsplit("/",maxsplit=1)[0] + "/" + filename
+        outpath = dev_datapath.rsplit("/",maxsplit=1)[0] + "/" + filename
 
-    outfiledev = open(outpathdev,'w',encoding='utf8')
-    indatadev = Data_processor(inpathdev)
-    totaldev = len(indatadev.data)
+    outfile = open(outpath, 'w', encoding='utf8')
+    training_data = Data_processor(training_datapath).data
+    dev_data = Data_processor(dev_datapath)
+    total_tweets = len(dev_data.data)
+
     transAB_dict = {}
     transABC_dict = {}
     emis_dict = {}
-    for tweet in range(len(indatadev.data)):
+
+    for tweet in range(total_tweets):
         score_dict = {}
-        opYseq = viterbiTrigram_end(indatadev.data[tweet],emis_dict,transAB_dict,transABC_dict,Data,score_dict)
-        for i in range(len(opYseq[0].split(" "))):
-            output = indatadev.datal[tweet][i] + " " + opYseq[0].split(" ")[i] + "\n"
-            outfiledev.write(output)
-        outfiledev.write("\n")
-        print("dev " + str(tweet+1)+"/"+str(totaldev)+ " done")
-    print("Labelling done!")
-    outfiledev.close()
+        opYseq = viterbi_trigram_end(dev_data.data[tweet], emis_dict, transAB_dict, transABC_dict, training_data, score_dict)
+        tags = opYseq[0].split(" ")
+
+        for word in range(len(tags)):
+            output = dev_data.datal[tweet][word] + " " + tags[word] + "\n"
+            outfile.write(output)
+
+        outfile.write("\n")
+        print(str(tweet+1) + "/" + str(total_tweets) + " done")
+
+    print("Labelling completed!")
+    outfile.close()
 
 
-def viterbiTrigram_start(sequence,i,emis_dict,trans_dict,Data,score_dict):
-    if (len(sequence),i) in score_dict.keys():
-        return score_dict[(len(sequence),i)]
+def viterbi_trigram_start(sequence, state, emis_dict, trans_dict, training_data, score_dict):
+    if (len(sequence), state) in score_dict.keys():
+        return score_dict[(len(sequence), state)]
     else:
-        score = transAB_prob("start",i,Data,trans_dict)*emis_prob(i,sequence[-1],Data,emis_dict)
-        score_dict[(len(sequence),i)] = [(i,score)]
-        return [(i,score)]
+        score = transAB_prob("start", state, training_data, trans_dict) * emis_prob(state, sequence[-1], training_data, emis_dict)
+        score_dict[(len(sequence), state)] = [(state, score)]
+        return [(state, score)]
 
-def viterbiTrigram_end(sequence,emis_dict,transAB_dict,transABC_dict,Data,score_dict):
-    maxY = ""
-    maxScore = 0
-    for i in possible_states:
+
+def viterbi_trigram_end(sequence, emis_dict, transAB_dict, transABC_dict, training_data, score_dict):
+    max_y = ""
+    max_score = 0
+
+    for state in possible_states:
         if len(sequence) == 1:
-            previous_max = viterbiTrigram_start(sequence,i,emis_dict,transAB_dict,Data,score_dict)
-            # print(previous_max)
-            score = previous_max[0][1]*transAB_prob(i,"stop",Data,transAB_dict)
-            if score > maxScore:
-                maxY = previous_max[0][0]
-                maxScore = score
+            previous_max = viterbi_trigram_start(sequence, state, emis_dict, transAB_dict, training_data, score_dict)
+            score = previous_max[0][1] * transAB_prob(state, "stop", training_data, transAB_dict)
+            if score > max_score:
+                max_y = previous_max[0][0]
+                max_score = score
         else:
-            previous_list = viterbiTrigramRecursive(sequence,i,emis_dict,transAB_dict,transABC_dict,Data,score_dict)
+            previous_list = viterbi_trigram_recursive(sequence, state, emis_dict, transAB_dict, transABC_dict, training_data, score_dict)
             for j in previous_list:
-                score = j[1]*(0.9*transAB_prob(i,"stop",Data,transAB_dict)+0.1*trans_prob_ABC(j[0].split(" ")[-2],j,"stop",Data,transABC_dict))
-                if score > maxScore:
-                    maxY = j[0]
-                    maxScore = score
-    if maxY == "":
-        previous_O_list = viterbiTrigramRecursive(sequence,"O",emis_dict,transAB_dict,transABC_dict,Data,score_dict)
-        maxY = previous_O_list[0][0]
-        maxScore = 0
-    return (maxY,maxScore)
+                score = j[1] * (0.9 * transAB_prob(state, "stop", training_data, transAB_dict) + 0.1 * trans_prob_ABC(j[0].split(" ")[-2], j, "stop", training_data, transABC_dict))
+                if score > max_score:
+                    max_y = j[0]
+                    max_score = score
 
-def viterbiTrigramRecursive(sequence,i,emis_dict,transAB_dict,transABC_dict,Data,score_dict):
-    if (len(sequence),i) in score_dict.keys():
-        return score_dict[(len(sequence),i)]
+    if max_y == "":
+        previous_O_list = viterbi_trigram_recursive(sequence, "O", emis_dict, transAB_dict, transABC_dict, training_data, score_dict)
+        max_y = previous_O_list[0][0]
+        max_score = 0
+    return (max_y, max_score)
+
+
+def viterbi_trigram_recursive(sequence, state, emis_dict, transAB_dict, transABC_dict, training_data, score_dict):
+    if (len(sequence), state) in score_dict.keys():
+        return score_dict[(len(sequence), state)]
     else:
         state_list = []
-        for j in possible_states:
+        for prev_state in possible_states:
             if len(sequence) == 2:
-                previous_list = viterbiTrigram_start(sequence[:-1],j,emis_dict,transAB_dict,Data,score_dict)
+                previous_list = viterbi_trigram_start(sequence[:-1], prev_state, emis_dict, transAB_dict, training_data, score_dict)
             else:
-                previous_list = viterbiTrigramRecursive(sequence[:-1],j,emis_dict,transAB_dict,transABC_dict,Data,score_dict)
-            maxY = ""
-            maxScore = 0
+                previous_list = viterbi_trigram_recursive(sequence[:-1], prev_state, emis_dict, transAB_dict, transABC_dict, training_data, score_dict)
+
+            max_y = ""
+            max_score = 0
+
             for k in previous_list:
                 if len(sequence) == 2:
-                    maxScore = k[1]*transAB_prob(j,i,Data,transAB_dict)*emis_prob(i,sequence[-1],Data,emis_dict)
-                    maxY = k[0] + " " + i
+                    max_score = k[1] * transAB_prob(prev_state, state, training_data, transAB_dict) * emis_prob(state, sequence[-1], training_data, emis_dict)
+                    max_y = k[0] + " " + state
                 else:
-                    score = k[1]*(0.9*transAB_prob(j,i,Data,transAB_dict)+0.1*trans_prob_ABC(k[0].split(" ")[-2],j,i,Data,transABC_dict))*emis_prob(i,sequence[-1],Data,emis_dict)
-                    if score > maxScore:
-                        maxY = k[0] + " " + i
-                        maxScore = score
-            if maxY == "":
-                maxY = previous_list[0][0] + " " + i
-            state_list.append((maxY,maxScore))
-        score_dict[(len(sequence),i)] = state_list
+                    score = k[1] * (0.9 * transAB_prob(prev_state, state, training_data, transAB_dict) + 0.1 * trans_prob_ABC(k[0].split(" ")[-2], prev_state, state, training_data, transABC_dict)) * emis_prob(state, sequence[-1], training_data, emis_dict)
+                    if score > max_score:
+                        max_y = k[0] + " " + state
+                        max_score = score
+
+            if max_y == "":
+                max_y = previous_list[0][0] + " " + state
+            state_list.append((max_y, max_score))
+
+        score_dict[(len(sequence), state)] = state_list
         return state_list
 
+
 if len(sys.argv) < 5:
-    print("Not enough arguments pls input in order: (input data file path, training data file path, name of file,'W'(for Windows) or 'L'(for Linux/Mac)")
+    print("Not enough arguments pls input in order: (input data file path, training data file path, filename, 'W'(for Windows) or 'L'(for Linux/Mac)")
     sys.exit()
 
 viterbip5_label(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
